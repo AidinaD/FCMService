3d5540dc5ac97ed1e5c89ca09820890371ca4787
unknown <duishekeeva.a@gmail.com>
2019-10-22 00:04:36 +0800

    first commit

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php
+	define('DB_USERNAME','root');
+	define('DB_PASSWORD','');
+	define('DB_NAME','test_fcm');
+	define('DB_HOST','localhost');
+
+	//defined a new constant for firebase api key
+	define('FIREBASE_API_KEY', 'AIzaSyAya1gjGv9nXGo_rOLu_9dB3I5IVpLt4Xs');
